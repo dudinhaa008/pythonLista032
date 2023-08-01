50e380fe61f8b817cc29d9de1348959c8eaf1db1
dudinhaa008 <mariaeaaraujo@gmail.com>
2023-08-01 11:34:41 -0300

    primeiro commit

---
+++ PraticaPython/ex_fixacao01.py
@@ -0,0 +1,9 @@
+"""
+
+"""
+
+num = float(input("Informe um valor em dólraes para ser convertido: "))
+
+num = num * 4.78
+
+print("O valor em reais é:", num)
---
+++ PraticaPython/ex_fixacao02.py
@@ -0,0 +1,7 @@
+conta = float(input("informe o valor da conta: "))
+
+acrescimo = conta * 10 / 100
+
+total = conta + acrescimo
+
+print(total)
